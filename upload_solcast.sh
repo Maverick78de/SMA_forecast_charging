50d41edae1b3c24f2a034410feb31e65ce545a2c
Maverick78de <mboe78@gmail.com>
2020-04-27 11:52:38 +0200

    added PATH t

--- upload_solcast.sh
+++ upload_solcast.sh
@@ -3,6 +3,7 @@
 #MIT License - see LICENSE.md 
 #Copyright (c) [2020] [Matthias Boettger <mboe78@gmail.com>]
 #
+PATH=/usr/local/bin:/usr/bin:/bin
 
 site_id=XXXX-XXXX-XXXX-XXXX
 key_id=yyyyyyyyyyyyyyyyyyyyyyyyyyy
