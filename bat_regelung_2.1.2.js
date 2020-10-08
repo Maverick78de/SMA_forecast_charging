3240b8b8138a7efc802baf1efb2a4c1e33f44f6c
Maverick78de <mboe78@gmail.com>
2020-10-08 07:52:07 +0200

    Update and rename bat_regelung_2.1.1.js to bat_regelung_2.1.2.js
    benötigt neue get_solcast!

--- bat_regelung_2.1.2.js
+++ bat_regelung_2.1.2.js
@@ -2,7 +2,7 @@
 MIT License - see LICENSE.md 
 Copyright (c) [2020] [Matthias Boettger <mboe78@gmail.com>]
 */
-/*Version 2.1.1*/
+/*Version 2.1.2*/
 // Debug
 var debug = 1; /*debug ausgabe ein oder aus 1/0 */
 
